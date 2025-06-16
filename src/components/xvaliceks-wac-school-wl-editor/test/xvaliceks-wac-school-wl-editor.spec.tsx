8b6783f15ba069e3073a9c8ed77fb491a43d99dc
Simon Valicek <simon.valicek@posam.sk>
2025-06-16 11:39:31 +0200

    dasdas

--- src/components/xvaliceks-wac-school-wl-editor/test/xvaliceks-wac-school-wl-editor.spec.tsx
+++ src/components/xvaliceks-wac-school-wl-editor/test/xvaliceks-wac-school-wl-editor.spec.tsx
@@ -2,17 +2,20 @@ import { newSpecPage } from '@stencil/core/testing';
 import { XvaliceksWacSchoolWlEditor } from '../xvaliceks-wac-school-wl-editor';
 
 describe('xvaliceks-wac-school-wl-editor', () => {
-  it('renders', async () => {
+  it('renders input fields and buttons', async () => {
     const page = await newSpecPage({
       components: [XvaliceksWacSchoolWlEditor],
       html: `<xvaliceks-wac-school-wl-editor></xvaliceks-wac-school-wl-editor>`,
     });
-    expect(page.root).toEqualHtml(`
-      <xvaliceks-wac-school-wl-editor>
-        <mock:shadow-root>
-          <slot></slot>
-        </mock:shadow-root>
-      </xvaliceks-wac-school-wl-editor>
-    `);
+
+    const shadow = page.root.shadowRoot;
+
+    expect(shadow.querySelectorAll('md-filled-text-field').length).toBe(3);
+    expect(shadow.querySelector('md-slider')).not.toBeNull();
+    expect(shadow.querySelector('md-filled-select')).not.toBeNull();
+    expect(shadow.querySelector('md-divider')).not.toBeNull();
+    expect(shadow.querySelector('md-filled-button')).not.toBeNull();
+    expect(shadow.querySelector('md-filled-tonal-button')).not.toBeNull();
+    expect(shadow.querySelector('md-outlined-button')).not.toBeNull();
   });
 });
